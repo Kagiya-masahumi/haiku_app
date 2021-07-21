fa058f351c76cf63499fb51a0bb02aa219300572
shouxingzi <79133015+shouxingzi@users.noreply.github.com>
2021-07-21 15:12:54 +0900

    ビューの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name
+  validates :name, presence: true
   # 6文字以上の半角英数字の制限
   validates :password,length: { minimum: 6 }, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/ }
 
