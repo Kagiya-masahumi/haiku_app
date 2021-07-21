33e27bcf5a24b927a1aeeaa9d24efe2830c32eb2
shouxingzi <79133015+shouxingzi@users.noreply.github.com>
2021-07-21 14:42:55 +0900

    userモデル　バリテーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name
+  # 6文字以上の半角英数字の制限
+  validates :password,length: { minimum: 6 }, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/ }
+
 end
+
+
