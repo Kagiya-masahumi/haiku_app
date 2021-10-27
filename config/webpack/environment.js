7ece329e74c7ce2478cbf6480e76e407f5b2f5a6
Kagiya-masahumi <Kagiya-masahumi@users.noreply.github.com>
2021-10-27 21:07:45 +0900

    『※必須項目です』の追記

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,12 +1,22 @@
 const { environment } = require('@rails/webpacker')
-const webpack = require('webpack')
+// const webpack = require('webpack')
 
+// environment.plugins.prepend('Provide',
+//   new webpack.ProvidePlugin({
+//     $: 'jquery',
+//     jQuery: 'jquery',
+//     jquery: 'jquery',
+//   })
+// )
+
+// 以下追記
+const webpack = require('webpack')
 environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    jquery: 'jquery',
-  })
+    new webpack.ProvidePlugin({
+        $: 'jquery/src/jquery',
+        jQuery: 'jquery/src/jquery'
+    })
 )
+// ここまで
 
 module.exports = environment
